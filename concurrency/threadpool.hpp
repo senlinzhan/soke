00a7d7c6cee043eeb25381bbaaf02500d530aa7c
senlinzhan <senlinzhan@gmail.com>
2017-02-25 23:24:57 +0800

    add threadpool cpp code

--- concurrency/threadpool.hpp
+++ concurrency/threadpool.hpp
@@ -1,9 +1,12 @@
 #ifndef THREADPOOL_HPP
 #define THREADPOOL_HPP
 
-#include <queue>
-#include <mutex>
+#include <chrono>
 #include <condition_variable>
+#include <mutex>
+#include <queue>
+#include <thread>
+
 
 typedef struct threadpool_task_t
 {
@@ -22,7 +25,10 @@ public:
     ThreadPool(const ThreadPool &) = delete;
     ThreadPool &operator=(const ThreadPool &) = delete;
 
+    void add(void (*function)(void *), void *argument);        
 private:
+    void worker();
+    
     int maxThreads_;
     int currentThreads_;
     int idleThreads_;
@@ -42,5 +48,93 @@ ThreadPool::ThreadPool(int maxThreads)
 {    
 }
 
+
+void ThreadPool::worker()
+{
+    while (true)
+    {
+        bool has_timedout = false;
+        {
+            std::lock_guard<std::mutex> guard(mutex_);
+            idleThreads_++;
+        }
+        std::unique_lock<std::mutex> uni_lock(mutex_);
+        
+        while (tasks_.empty() || !quit_)
+        {
+            if (cond_.wait_for(uni_lock, std::chrono::seconds(2)) == std::cv_status::timeout)
+            {
+                has_timedout = true;
+                break;
+            }
+        }
+        
+        {
+            mutex_.lock();
+            idleThreads_--;
+            if (quit_)
+            {
+                currentThreads_--;
+                mutex_.unlock();
+                quit_cond.notify_all();
+                return;
+            }
+
+            if (!tasks_.empty())
+            {
+                auto task = tasks_.front();
+                tasks_.pop();
+                task.function(task.argument);
+                mutex_.unlock();
+            }
+            if (tasks_.empty() && has_timedout)
+            {
+                currentThreads_--;
+                return;
+            }
+        }
+    }
+}
+
+
+void ThreadPool::add(void (*function)(void *), void *argument)
+{
+    threadpool_task_t task;
+    task.function = function;
+    task.argument = argument;
+
+    std::lock_guard<std::mutex> guard(mutex_);
+    
+    tasks_.push(task);
+    if (idleThreads_ > 0)
+    {
+        cond_.notify_one();
+    }
+    else if (currentThreads_ < maxThreads_)
+    {
+        std::thread t(&ThreadPool::worker, this);
+        currentThreads_++;
+    }
+}
+
+
+ThreadPool::~ThreadPool()
+{
+    std::lock_guard<std::mutex> guard(mutex_);
+    std::unique_lock<std::mutex> uni_lock(mutex_);
+    
+    if (currentThreads_ > 0)
+    {
+        quit_ = true;
+        if (idleThreads_ > 0)
+        {
+            cond_.notify_all();
+        }
+        while (currentThreads_ > 0)
+        {
+            quit_cond.wait(uni_lock);            
+        }
+    }    
+}
 
 #endif /* THREADPOOL_H */
