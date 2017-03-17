946b2ff5b41d4516f0f98a7e46e972d37760c1e3
senlinzhan <senlinzhan@gmail.com>
2017-03-18 01:33:47 +0800

    IPv6 support

--- IPAddress.cpp
+++ IPAddress.cpp
@@ -2,13 +2,61 @@
 
 #include <utility>
 #include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
 #include <vector>
 
 using std::vector;
 
-IPAddress::IPAddress(const sockaddr_storage &addr)
-    : addr_(addr)
+IPAddress::IPAddress(const std::string &host, uint16_t port)
+    : host_(host), port_(port)
 {
+    struct addrinfo hints;
+    bzero(&hints, sizeof(hints));
+    hints.ai_flags = AI_NUMERICHOST;
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    struct addrinfo *result = nullptr;
+    int ret = getaddrinfo(host.c_str(), NULL, &hints, &result);
+    if (ret != 0)
+    {
+        LOG(ERROR) << "IPAddress::IPAddress - invalid argument host: " << host;
+        return;        
+    }
+
+    if (result->ai_family == AF_INET)
+    {
+        type_ = Type::ipv4;
+    }
+    else if (result->ai_family == AF_INET6)
+    {
+        type_ = Type::ipv6;
+    }
+    else
+    {
+        LOG(ERROR) << "IPAddress::IPAddress - invalid argument host: " << host;
+    }
+}
+
+IPAddress::IPAddress(const sockaddr_storage &addr)
+{
+    if (addr.ss_family == AF_INET)
+    {
+        type_ = Type::ipv4;
+    }
+    else if (addr.ss_family == AF_INET6)
+    {
+        type_ = Type::ipv6;
+    }
+    else
+    {
+        LOG(ERROR) << "IPAddress::IPAddress() - invalid argument addr";
+        return;
+    }
+    
     unsigned short port;    
     const char *ptr = nullptr;
 
@@ -16,42 +64,33 @@ IPAddress::IPAddress(const sockaddr_storage &addr)
     vector<char> clientAddress(addrLen);    
     
     if (isIPv4())
-    {
-        auto addr = reinterpret_cast<sockaddr_in *>(const_cast<sockaddr_storage *>(&addr_));	
-	ptr = inet_ntop(AF_INET, &(addr->sin_addr), &clientAddress[0], INET_ADDRSTRLEN);
-	port = ntohs(addr->sin_port);
+    { 
+        auto sockaddr = reinterpret_cast<sockaddr_in *>(const_cast<sockaddr_storage *>(&addr));	
+	ptr = inet_ntop(AF_INET, &(sockaddr->sin_addr), clientAddress.data(), INET_ADDRSTRLEN);
+	port_ = ntohs(sockaddr->sin_port);
     }
     else
     {
-	auto addr = reinterpret_cast<sockaddr_in6 *>(const_cast<sockaddr_storage *>(&addr_));
-	ptr = inet_ntop(AF_INET6, &(addr->sin6_addr), &clientAddress[0], INET6_ADDRSTRLEN);
-	port = ntohs(addr->sin6_port);	
+	auto sockaddr = reinterpret_cast<sockaddr_in6 *>(const_cast<sockaddr_storage *>(&addr));
+	ptr = inet_ntop(AF_INET6, &(sockaddr->sin6_addr), clientAddress.data(), INET6_ADDRSTRLEN);
+	port_ = ntohs(sockaddr->sin6_port);	
     }
-
     if (ptr == nullptr)
     {
-	throw IPAddressFormatError("the format of ip address is invalid");	
-    }
-    
-    ip_ = std::string(ptr);
-    port_ = port;
+        LOG(ERROR) << "IPAddress::IPAddress() - invalid ip address";
+        return;
+    }    
+    host_ = std::string(ptr);
 }
-
 
 bool IPAddress::isIPv4() const
 {
-    return addr_.ss_family == AF_INET;    
+    return type_ == Type::ipv4;
 }
 
 bool IPAddress::isIPv6() const
 {
-    return addr_.ss_family == AF_INET6;    
-}
- 
-
-size_t IPAddress::size() const
-{  
-    return size_;    
+    return type_ == Type::ipv6;
 }
 
 std::string IPAddress::toString() const
@@ -61,7 +100,7 @@ std::string IPAddress::toString() const
 
 std::string IPAddress::IPAddr() const
 {
-    return ip_;    
+    return host_;    
 }
 
 unsigned short IPAddress::port() const
--- IPAddress.hpp
+++ IPAddress.hpp
@@ -3,40 +3,17 @@
 
 #include <netinet/in.h>
 #include <string>
-#include <exception>
-
-class IPAddressFormatError : public std::exception
-{
-public:
-    IPAddressFormatError(const std::string &msg)
-        : msg_(msg)
-    {	
-    }
-    
-    IPAddressFormatError(std::string &&msg) noexcept
-        : msg_(std::move(msg))
-    {	
-    }
-    
-    virtual const char * what() const noexcept override    
-    {        
-        return msg_.c_str();        
-    }
-    
-private:
-    std::string msg_;
-};
-
 
 class IPAddress
 {
 public:
+    enum Type { ipv4, ipv6 };
+    
+    IPAddress(const std::string &host, uint16_t port);
     IPAddress(const sockaddr_storage &addr);
     
     bool isIPv4() const;
     bool isIPv6() const;    
-
-    size_t size() const;    
 
     std::string IPAddr() const;
     unsigned short port() const;
@@ -44,13 +21,9 @@ public:
     std::string toString() const;    
 
 private:
-    static constexpr size_t size_ = sizeof(sockaddr_storage);
-    
-    sockaddr_storage addr_;
-    std::string ip_;
-    unsigned short port_;    
+    Type        type_;
+    std::string host_;
+    uint16_t    port_;
 };
 
-
 #endif /* IPADDRESS_H */
- 
